193b4ca60448c654b6c78c99d563994d91f83903
jormais <jonaybrito15@gmail.com>
2022-08-22 23:10:01 +0100

    s ehan añadido los componentes footer y header a la página

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  opacity: string = 'opacity: 0.5;';
+  constructor() { 
+    fromEvent(document, 'scroll').subscribe( () => {
+      this.opacity = (document.documentElement.scrollTop > 720)
+          ? 'opacity: 1;'
+          : 'opacity: 0.5;'
+    });
+  }
 
   ngOnInit(): void {
   }
