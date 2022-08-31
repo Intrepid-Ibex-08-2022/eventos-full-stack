df037812842c43851ef3ba8cb21e301bf19f8a77
Juan Carlos <jcvargas273@gmail.com>
2022-08-31 22:44:14 +0100

    prueba con imagenes rotas

--- src/app/services/events/filters.service.ts
+++ src/app/services/events/filters.service.ts
@@ -7,12 +7,13 @@ import axios from 'axios';
 })
 export class FiltersService {
   eventosCanarios: EventsResult[] = [];
-  urlEventos = 'http://localhost:3000' + '/eventosCanarios';
+  urlEventos = 'http://localhost:3000' + '/eventosCanariosFallos';
   // urlEventos =
   //   'mongodb+srv://intrepidibex:Stos5BsCqdS7MzIb@cluster0.lxr4zbx.mongodb.net/?retryWrites=true&w=majority' +
   //   '/eventosCanarios';
   // urlEventos =
   // 'https://happy-hats-rush-92-172-244-82.loca.lt' + '/eventosCanarios'; // Este portal es el mismo que mi local http://localhost:3000/eventosCanarios pero declarado público con el local tunnel. Los demas usuarios de la app ascederán a mi localhost3000 con la dirreccion que les ponga aquí.
+
   constructor() {}
 
   async getEvents(): Promise<EventsResult[]> {
