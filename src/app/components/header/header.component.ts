39fa9f6125bb0449569b56f245a2cf24f6824673
Juan Carlos <jcvargas273@gmail.com>
2022-09-03 09:39:09 +0100

    username e icono apareciendo al loguearse, agreagado boton logout, permanecia de datos del usuario al navegar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 
 @Component({
@@ -7,12 +8,45 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit {
   @Input() position = 'position: fixed;'
+  id: string | null = '';
+  username: string | undefined;
 
+  constructor(
+    private router: Router,
+    private authServices: AuthService
+  ){}
 
-  constructor(private router: Router){}
+  async ngOnInit(): Promise<void> {
+    this.id = localStorage.getItem('token');
+     if(this.id){
+       (await this.authServices.loginId(this.id)).subscribe( resp =>{
+         if(resp !== undefined){
+           this.username = resp
+         }
+         return;
+       })
+
+     }
+
+  }
+
+  loginValido(){
+    if(this.username){
+      return true
+    }else{
+      return false
+    }
+  }
+
   navigate(ruta: string){
     this.router.navigateByUrl(`/${ruta}`)
+  }
+
+  logOut(){
+    localStorage.removeItem('token');
+    this.username = undefined;
+
   }
 }
