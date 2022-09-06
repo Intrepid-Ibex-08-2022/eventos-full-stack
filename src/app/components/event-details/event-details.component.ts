33b274e25f2c8327359e90eb7ddd5b0a504b314d
Juan Carlos <jcvargas273@gmail.com>
2022-09-06 23:42:01 +0100

    conexion a google maps, dando ubicacion real del evento

--- src/app/components/event-details/event-details.component.ts
+++ src/app/components/event-details/event-details.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { GetEventsService } from '../../services/events/get-events.service';
 import { EventsResult } from '../../interface/event';
 import { ActivatedRoute } from '@angular/router';
+import { fromEvent } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+
 
 @Component({
   selector: 'app-event-details',
@@ -10,20 +13,66 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EventDetailsComponent implements OnInit {
   event: EventsResult | undefined;
+  sizeWidth: string = '930';
+  position = {
+    lat: 28.12462338053807,
+    lng: -15.437557770012095
+  }
+  label ={
+    color: 'blue',
+    text: 'Ubicación',
+    opacity: 0.8
+  }
+  zoom=12
+
 
   constructor(
     private eventServices: GetEventsService,
     private activateRoute: ActivatedRoute,
-  ) {}
+    private responsive: BreakpointObserver
+  ) { }
 
   ngOnInit(): void {
     const id = this.activateRoute.snapshot.paramMap.get('id') as string;
     this.getEvent(id);
+
+    this.sizeMap()
   }
+
+  sizeMap(): void {
+    this.responsive.observe([
+      Breakpoints.Medium,
+      Breakpoints.Small,
+      Breakpoints.XSmall
+    ])
+    .subscribe(result => {
+
+      let breakpoints = result.breakpoints
+
+      if (breakpoints[Breakpoints.Large] || breakpoints[Breakpoints.Medium] ) {
+        this.sizeWidth = '930'
+      }else if (breakpoints[Breakpoints.Small] || breakpoints[Breakpoints.Small]){
+        this.sizeWidth = '430'
+      }
+    });
+  }
+
+  eventPostion(){
+    let urlMap = this.event?.map_link;
+
+    if(urlMap){
+      let position = urlMap?.substr(urlMap.search('@')+1, 22);
+      let lat = position?.substr(0, position.search(','));
+      let lng = position?.substr(position.search(',')+1, position.length);
+
+      this.position = {lat: parseFloat(lat), lng:parseFloat(lng)}
+    }
+  };
 
   async getEvent(id: string) {
     (await this.eventServices.getEventDetails(id)).subscribe((resp) => {
       this.event = resp;
+      this.eventPostion()
     });
   }
 }
