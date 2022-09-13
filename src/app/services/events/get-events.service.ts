331205ad29bd14d4fef6a8a84064034613858e23
Picigui <picigui@hotmail.com>
2022-09-13 12:20:10 +0100

    añadir icono favicon

--- src/app/services/events/get-events.service.ts
+++ src/app/services/events/get-events.service.ts
@@ -9,20 +9,19 @@ import { Users } from '../../interface/users';
   providedIn: 'root',
 })
 export class GetEventsService {
-  url = 'https://intrepit-ibex.herokuapp.com/api/events/event';
+  // url = 'https://intrepit-ibex.herokuapp.com/api/events/event';
+  url = 'http://localhost:4000/api/users';
 
-  constructor(
-    private http: HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
-  async getEventDetails(id: string): Promise<Observable<EventsResult | undefined>> {
-    let event: Observable< EventsResult | undefined>;
-    event = await this.http.get<EventsResult>(`${this.url}/${id}`)
-    .pipe(
+  async getEventDetails(
+    id: string,
+  ): Promise<Observable<EventsResult | undefined>> {
+    let event: Observable<EventsResult | undefined>;
+    event = await this.http.get<EventsResult>(`${this.url}/${id}`).pipe(
       map((resp) => {
-
-      return resp;
-      })
+        return resp;
+      }),
     );
     return event;
   }
